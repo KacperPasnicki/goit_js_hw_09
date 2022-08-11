b6ae92fb0f3b397720b6450395f1296dea7e0e69
KacperPasnicki <kacper.pas@hotmail.com>
2022-08-11 20:40:52 +0200

    3.0

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,23 +1,33 @@
 import Notiflix from 'notiflix'
+const btn = document.querySelector('button')
 
-function createPromise = (position, delay) => {
-  return ...
+
+const createPromise = (position, delay) => {
+  return  new Promise((resolve, reject) => {
+    setTimeout(() => {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill resolve...
+    // Fulfill 
+    resolve ({position, delay})
   } else {
-    // Reject reject...
+    // Reject 
+    reject ({position, delay})
   }
-}
+  }, delay) 
+})
+
+} 
 
 
 
-form.addEventListener....
+btn.addEventListener('click', e => {
+  e.preventDefault();
+ createPromise(2, 1500)
+   .then(({ position, delay }) => {
+     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+   })
+   .catch(({ position, delay }) => {
+     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+   });
 
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
   })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
