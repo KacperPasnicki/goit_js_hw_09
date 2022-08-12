74f399400a831b33074e0f15451fa4e14df85a5e
KacperPasnicki <kacper.pas@hotmail.com>
2022-08-12 23:05:47 +0200

    3.4(naprawa gh pages)

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,38 +1,8 @@
-import Notiflix from 'notiflix'
-const btn = document.querySelector('button')
-const form = document.querySelector('.form')
-const value =  Number(form.elements.amount.value);
-const delayMs =  Number(form.elements.delay.value);
-const stepMs =  Number(form.elements.step.value);
-
-const createPromise = (position, delay) => {
-  return  new Promise((resolve, reject) => {
-    setTimeout(() => {
+function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
-    // Fulfill 
-    resolve ({position, delay})
+    // Fulfill
   } else {
-    // Reject 
-    reject ({position, delay})
+    // Reject
   }
-  }, delay) 
-})
-
-} 
-
-
-
-btn.addEventListener('click', e => {
-  e.preventDefault();
-  for  (let i = 1; i <= value ; i+=1 ) {
-    let stepTime = delayMs + stepMs * (i-1);
-      createPromise(i, stepTime)
-        .then(({ position, delay }) => {
-          Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
-      })
-      .catch(({ position, delay }) => {
-        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
-      });
-      }
-      })
+}
